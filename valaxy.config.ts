ac027288508e0c00dc0d4e9bcee76015d42a65fb
kiyotakali <924120642@qq.com>
2024-09-11 18:22:33 +0800

    change back_pic

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -12,7 +12,28 @@ export default defineConfig<ThemeConfig>({
   url: 'http://kiyotakali.top/',
   themeConfig:{
     theme: 'dark',
-    
+    home: {
+      title:"kiyotakali's blog",
+      headline:"kiyotakali's Blog",
+      description:"My name is kiyotakali, Ciallo～(∠・ω< )⌒☆.",
+      images: [
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/atri.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/bocchi_the_rock.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/color.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/genshin.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/hollow_knight.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/li.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/miku1.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/miku3.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/shanjing.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/water_world.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/windows.webp",
+        "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/yireyna.webp",
+        "https://pic.axi404.top/93141803_p0.ic1wc5z17.webp",
+        "https://pic.axi404.top/93141612_p0.54xox14iok.webp",
+        "https://raw.githubusercontent.com/kiyotakali/kiyotakali.top/main/pic_back/2.png",
+      ]
+    },
     nav: [
       {
         text: 'Home',
