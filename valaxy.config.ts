5b32283cb02bfaae6ada1fd102d6a02b98379f41
kiyotakali <924120642@qq.com>
2024-09-12 18:07:07 +0800

    delete back

--- valaxy.config.ts
+++ valaxy.config.ts
@@ -20,7 +20,6 @@ export default defineConfig<ThemeConfig>({
         "https://raw.githubusercontent.com/kiyotakali/kiyotakali.top/main/pic_back/2.webp",
         "https://raw.githubusercontent.com/kiyotakali/kiyotakali.top/main/pic_back/3.webp",
         "https://raw.githubusercontent.com/kiyotakali/kiyotakali.top/main/pic_back/4.webp",
-        "https://raw.githubusercontent.com/kiyotakali/kiyotakali.top/main/pic_post/ba_1.webp",
         "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/atri.webp",
         "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/bocchi_the_rock.webp",
         "https://raw.githubusercontent.com/xjtu-wjz/void2004/main/pics/color.webp",
